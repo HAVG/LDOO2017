04e7612c5261c51e2ed3cd6bbb9b83d35247acaa
mysshell <d.a.h.5@hotmail.com>
2017-02-20 16:13:02 -0600

    Trabajo incompleto, error en javascript, corregir para proceder

--- Week5/Assignment/web/Check.js
+++ Week5/Assignment/web/Check.js
@@ -1,14 +1,46 @@
-function check()
-{
-    var name = document.getElementsByName(txt-name).value;
-    var email = document.getElementsByName(txt-email).value;
-    var username = document.getElementsByName(txt-username).value;
-    var whoami = document.getElementsByName(whoami).value;
+function check() {
 
-    name = encodeURI(name);
-    email = encodeURI(email);
-    username = encodeURI(username);
-    whoami = encodeURI(whoami);
+    const name = document.getElementsByName("txt-name").value;
+    const email = document.getElementsByName("txt-email").value;
+    const username = document.getElementsByName("txt-user").value;
+    const password = document.getElementsByName("password").value;
+    const bm = document.getElementsByName("bm").value;
+    const gender = document.getElementsByName("gender").value;
+    const statements = [name, email, username, password, bm, gender];
 
-    name.innerHTML();
+    const forbidenStatements = [null, '<', '>', '=', '|', '&', '%', '?', '!' ];
+    let accessDenied = false;
+    for(let i = 0; i < statements.length; i++)
+    {
+        for(let j = 0; j < statements[i].length; j++)
+        {
+            if(statements[i] == "" || statements == null)
+            {
+                for (let l = 0; l < forbidenStatements.length; l++)
+                {
+                    if (statements[i].charAt(j) == forbidenStatements[l]) {
+                        accessDenied = true;
+                    }
+                }
+            }
+            else
+            {
+                accessDenied = true;
+            }
+        }
+    }
+    if(accessDenied == true)
+    {
+        document.body.innerHTML = '';
+        document.body.innerHTML += "<h1>Acceso denegado, ha introducido un caracter invalido.</h1>";
+    }
 }
+
+/*if(statements[i].charAt(j) == null || statements[i].charAt(j) == '<'
+    || statements[i].charAt(j) == '>' || statements[i].charAt(j) == '='
+    || statements[i].charAt(j) == '|' || statements[i].charAt(j) == '&'
+    || statements[i].charAt(j) == '%' || statements[i].charAt(j) == '')
+{
+
+}
+*/
